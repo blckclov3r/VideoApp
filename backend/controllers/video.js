ebcb0a200b45a67de1abef0f0ea0673779bc144b
blckclov3r <blckclov3r@gmail.com>
2022-07-28 14:16:45 +0800

    fixed upload

--- backend/controllers/video.js
+++ backend/controllers/video.js
@@ -2,15 +2,16 @@ import User from "../models/User.js";
 import Video from "../models/Video.js";
 import { createError } from "../error.js";
 
-export const addVideo = async(req,res,next)=>{
- try {
-    const newVideo = new Video({userId: req.user.id,...req.body})
-    const savedVideo = await newVideo.save();
-    res.status(201).json(savedVideo)
- } catch (error) {
-    next(error);
- }   
-}
+export const addVideo = async (req, res, next) => {
+   const newVideo = new Video({ userId: req.user.id, ...req.body });
+   try {
+     const savedVideo = await newVideo.save();
+     res.status(200).json(savedVideo);
+   } catch (err) {
+      console.log('addVideo',err);
+     next(err);
+   }
+ };
 
 export const updateVideo = async(req,res,next)=>{
     try {
--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 import styled from "styled-components";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+import app from "../firebase";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
     width: 100%;
@@ -16,8 +22,8 @@ const Container = styled.div`
 const Wrapper = styled.div`
     width: 600px;
     height: 600px;
-    background: ${({theme})=>theme.bgLighter};
-    color: ${({theme})=>theme.text};
+    background: ${({ theme }) => theme.bgLighter};
+    color: ${({ theme }) => theme.text};
     padding: 1.4rem;
     display: flex;
     flex-direction: column;
@@ -39,15 +45,15 @@ const Title = styled.h1`
 `
 
 const Input = styled.input`
-    border: 1px solid ${({theme})=>theme.soft}
-    color: ${({theme})=>theme.text};
+    border: 1px solid ${({ theme }) => theme.soft}
+    color: ${({ theme }) => theme.text};
     border-radius: 3px;
     padding: 10px;
     background-color: transparent;
 `
 const Desc = styled.textarea`
-    border: 1px solid ${({theme})=>theme.soft};
-    color: ${({theme})=>theme.text};
+    border: 1px solid ${({ theme }) => theme.soft};
+    color: ${({ theme }) => theme.text};
     border-radius: 3px;
     padding: 10px;
     background-color: transparent;
@@ -59,8 +65,8 @@ border: none;
 padding: 10px 20px;
 font-weight: 500;
 cursor: pointer;
-border: 1px solid ${({theme})=>theme.soft};
-color: ${({theme})=>theme.text};
+border: 1px solid ${({ theme }) => theme.soft};
+color: ${({ theme }) => theme.text};
 `
 
 const Label = styled.label`
@@ -68,21 +74,123 @@ font-size: 14px;
 `
 
 
-export const Upload = ({setOpen}) => {
-  return (
-   <Container>
-    <Wrapper>
-        <Close onClick={()=>setOpen(false)}>X</Close>
-        <Title>Upload a video</Title>
-        <Label>Video:</Label>
-        <Input type="file" accept="video/*" />
-        <Input type="text" placeholder="Title" />
-        <Desc type="text" placeholder="Title" rows={8} />
-        <Input type="text" placeholder="Separate the tags with commas." />
-        <Label>Image:</Label>
-        <Input type="file" accept="image/*" />
-        <Button>Upload</Button>
-    </Wrapper>
-   </Container>
-  )
+export const Upload = ({ setOpen }) => {
+
+    const [image, setImage] = useState(null);
+    const [video, setVideo] = useState(null);
+    const [imagePercentage, setImagePercentage] = useState(0);
+    const [videoPercentage, setVideoPercentage] = useState(0);
+
+    const navigate = useNavigate();
+
+    const [inputs, setInputs] = useState({});
+    const [tags, setTags] = useState([]);
+
+    const handleTags = (e) => {
+        setTags(e.target.value.split(","));
+    }
+
+    const handleChange = (e) => {
+        setInputs(prev => {
+            return { ...prev, [e.target.name]: e.target.value }
+        });
+    }
+
+    const uploadFile = (file, urlType) => {
+
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+    
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            urlType === "imgUrl" ? setImagePercentage(Math.round(progress)) : setVideoPercentage(Math.round(progress));
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+              default:
+                break;
+            }
+          },
+          (error) => {},
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                setInputs((prev) => {
+                    return { ...prev, [urlType]: downloadURL };
+                  });
+            });
+          }
+        );
+      };
+
+      useEffect(() => {
+        video && uploadFile(video , "videoUrl");
+      }, [video]);
+    
+      useEffect(() => {
+        image && uploadFile(image, "imgUrl");
+      }, [image]);
+    
+      const handleUpload = async ()=>{
+        
+        const res = await axios.post("/videos", {...inputs, tags})
+        console.log('res ====>',res)
+        setOpen(false)
+        res.status===200 && navigate(`/video/${res.data._id}`)
+      }
+
+    return (
+        <Container>
+            <Wrapper>
+                <Close onClick={() => setOpen(false)}>X</Close>
+                <Title>Upload a New Video</Title>
+                <Label>Video:</Label>
+                {videoPercentage > 0 ? (
+                    "Uploading:" + videoPercentage
+                ) : (
+                    <Input
+                        type="file"
+                        accept="video/*"
+                        onChange={(e) => setVideo(e.target.files[0])}
+                    />
+                )}
+                <Input
+                    type="text"
+                    placeholder="Title"
+                    name="title"
+                    onChange={handleChange}
+                />
+                <Desc
+                    placeholder="Description"
+                    name="desc"
+                    rows={8}
+                    onChange={handleChange}
+                />
+                <Input
+                    type="text"
+                    placeholder="Separate the tags with commas."
+                    onChange={handleTags}
+                />
+                <Label>Image:</Label>
+                {imagePercentage > 0 ? (
+                    "Uploading:" + imagePercentage + "%"
+                ) : (
+                    <Input
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) => setImage(e.target.files[0])}
+                    />
+                )}
+                <Button onClick={handleUpload}>Upload</Button>
+            </Wrapper>
+        </Container>
+    )
 }
