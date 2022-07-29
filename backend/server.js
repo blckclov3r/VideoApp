333ac7e5af23f813f136ef3f7946e0bc43844f0d
blckclov3r <blckclov3r@gmail.com>
2022-07-29 19:02:56 +0800

    deploy

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,10 @@ import {fileURLToPath} from 'url';
 dotenv.config();
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
