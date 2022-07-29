598e782a25734d05b4c5f7359ca5068240d5fde3
blckclov3r <blckclov3r@gmail.com>
2022-07-30 03:46:27 +0800

    deploy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://videoappclone.herokuapp.com/api',
     headers: { 
         'Access-Control-Allow-Origin' : '*'
     }
