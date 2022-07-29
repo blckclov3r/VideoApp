2d49e112a01243ae6c2cce7c3db1def8cbb72714
blckclov3r <blckclov3r@gmail.com>
2022-07-29 19:11:58 +0800

    commit changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://ytclonevideoapp.herokuapp.com/api',
-    withCredentials: true,
+    baseURL: 'http://ytclonevideoapp.herokuapp.com/api',
     headers: { 
         'Access-Control-Allow-Origin' : '*'
     }
